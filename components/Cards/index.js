85f52dac5d20eaf370f83deca44c4c0323191335
Tyree LeBeau <tyreelebeau24@gmail.com>
2019-07-21 22:10:46 -0500

    finished MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,80 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//Given URL for articles
+let articleUrl = "https://lambda-times-backend.herokuapp.com/articles";
+
+function getArticleData(articleUrl) {
+  // Get article data from url
+  axios
+    .get(articleUrl)
+    .then(response => {
+      // Get articles
+      let articleContent = [];
+      articleContent = response["data"]["articles"];
+
+      let articleName = "javascript";
+
+      createArticle(articleContent, articleName);
+
+      articleName = "bootstrap";
+
+      createArticle(articleContent, articleName);
+
+      articleName = "technology";
+
+      createArticle(articleContent, articleName);
+
+      articleName = "jquery";
+
+      createArticle(articleContent, articleName);
+
+      articleName = "node.js";
+
+      createArticle(articleContent, articleName);
+    })
+    .catch(error => {
+      console.log("Error:", error); // console logs errors
+    });
+}
+
+function createArticle(articleContent, articleName) {
+  let aHead = "headline";
+  let aName = "authorName";
+  let aPhoto = "authorPhoto";
+
+  for (i in articleContent[articleName]) {
+    //query select where to append
+    let container = document.querySelector(".cards-container");
+
+    // create class elements
+    let newDiv0 = document.createElement("div");
+    let newDiv1 = document.createElement("div");
+    let newDiv2 = document.createElement("div");
+    let newDiv3 = document.createElement("div");
+    let newSpan = document.createElement("span");
+    let newImg = document.createElement("img");
+
+    //create class lists
+    newDiv0.classList.add("card");
+    newDiv1.classList.add("headline");
+    newDiv2.classList.add("author");
+    newDiv3.classList.add("img-container");
+
+    //text content
+    newDiv1.innerText = articleContent[articleName][i][aHead];
+    newDiv2.innerText = articleContent[articleName][i][aName];
+    newImg.src = articleContent[articleName][i][aPhoto];
+
+    //append children
+    newDiv3.appendChild(newImg);
+    newDiv2.appendChild(newDiv3);
+    newDiv2.appendChild(newSpan);
+    newDiv1.appendChild(newDiv2);
+    newDiv0.appendChild(newDiv1);
+    container.appendChild(newDiv0);
+  }
+}
+
+getArticleData(articleUrl);
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,49 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//send the GET request
+let topicsUrl = "https://lambda-times-backend.herokuapp.com/topics";
+
+// Function used to get tab data
+function getTabData(topicsUrl) {
+  // Get tab data from url
+  axios
+    .get(topicsUrl)
+    .then(response => {
+      // Get topics
+      let topicContent = [];
+      topicContent = response["data"]["topics"];
+      topicContent.unshift("all");
+      console.log(topicContent);
+
+      // iterate through the array to create tabs
+      for (i in topicContent) {
+        CreateTabs(topicContent[i]);
+      }
+    })
+    .catch(error => {
+      console.log("Error:", error); //console logs errors
+    });
+}
+
+// Create function
+function CreateTabs(tabTopic) {
+  // query select where to append
+  let tab = document.querySelector(".topics");
+
+  // create element
+  let tabs = document.createElement("div");
+
+  //create class list
+  tabs.classList.add("tab");
+
+  //text content
+  tabs.innerText = tabTopic;
+
+  //append child
+  tab.appendChild(tabs);
+}
+
+// gets tab data
+getTabData(topicsUrl);
